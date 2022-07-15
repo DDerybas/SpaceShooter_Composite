100cd6101b7710b783531df50c35923fcf21bd48
DmitrijDerybas <samoylov.corp@gmail.com>
2022-07-15 19:23:48 +0300

    Added IInputModule interface

---
+++ Assets/Scripts/Modules/Interfaces/IInputModule.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using System;
+
+namespace Entities.Modules
+{
+    /// <summary>
+    /// Player input button type.
+    /// </summary>
+    public enum ButtonType
+    {
+        Action,
+        Cancel,
+        Pause
+    }
+
+    public interface IInputModule : IModule
+    {
+        /// <summary>
+        /// On axis action. Passes the input axis as Vector2.
+        /// </summary>
+        Action<Vector2> OnAxisAction { get; set; }
+
+        /// <summary>
+        /// On pressed/released button action. Passes the button type (ButtonType) and the 'pressed/released' condition (bool).
+        /// </summary>
+        Action<ButtonType, bool> OnButtonAction { get; set; }
+    }
+}
